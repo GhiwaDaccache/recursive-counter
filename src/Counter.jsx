26dc687854975019d3bc842621c4187c9d7529e2
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-03-24 20:16:10 +0200

    first-commit

---
+++ src/Counter.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {useState} from 'react';
+import "./styles/style.css"
+
+const Counter = () => {
+    const HandlePlus = () =>{
+        
+      };
+
+
+
+    return(
+    <div>
+        <div>
+          <span>-</span>
+          1
+          <button onClick={HandlePlus}>+</button>
+          <button>-</button>
+          <br />
+        </div>
+    </div>
+)
+}
+
+export default Counter; 
